9810b3d7cbaa216111fdf1f2c2f93cc7d244a81c
Rickard <rickard.laurin@gmail.com>
2015-01-16 14:29:14 +0100

    parse sl trip duration to a more readable string

--- services/bot.js
+++ services/bot.js
@@ -2,6 +2,8 @@
 
 var LastFm = require('./lastfm');
 var utils  = require('../utilities/utils');
+var moment  = require('moment');
+require('moment-duration-format');
 
 exports.service = function () {
   return {
@@ -100,44 +102,77 @@ exports.service = function () {
 
       var baseUrl = 'http://api.sl.se/api2/';
       var plats = baseUrl + 'typeahead.json?key={key}&searchstring={search}&stationsonly=true&maxresults=1';
-      var real = baseUrl + 'realtimedepartures.json?key={key}&siteid={id}'
-      var station = commands[2] ? commands[2] : 'Rådmansgatan';
+      var real = baseUrl + 'realtimedepartures.json?key={key}&siteid={id}';
+      var rese = baseUrl + 'TravelplannerV2/trip.json?key={key}&originId={origin}&destId={destination}';
+
+      var station = commands[1] ? commands[1] : 'Rådmansgatan';
       
       var keyPlats = process.env.SL_PLATS;
       var keyReal = process.env.SL_REAL;
+      var keyRese = process.env.SL_RESE;
 
-      plats = plats.replace('{key}', keyPlats).replace('{search}', station);
+      if (commands.length > 2) {
+        rese = rese.replace('{key}', keyRese);
+        rese = rese.replace('{origin}', commands[1]);
+        rese = rese.replace('{destination}', commands[2]);
 
-      utils.getDataFromURL(plats)
-        .then(function (body) {
-          if (body.StatusCode !== 0) {
-            channel.send('Något gick snett');
-            return;
-          }
+        var trip = [];
 
-          if (!body.ResponseData[0]) {
-            channel.send('Kan inte hitta någon station med namnet *' + station);
-            return;
-          }
+        utils.getDataFromURL(rese)
+          .then(function (body) {
+            var firstTrip = body.TripList.Trip[0];
 
-          var name = body.ResponseData[0].Name;
-          var id = body.ResponseData[0].SiteId;
+            if (firstTrip.LegList.Leg.length) {
+              firstTrip.LegList.Leg.forEach(function (leg) {
+                if (!leg.hide) {
+                  trip.push(leg.Origin.time + ' ' + leg.Origin.name + ' (' + leg.name + ')\n|');
+                  trip.push(leg.Destination.time + ' ' + leg.Destination.name + '\n');
+                }
+              });
+            } else {
+              var leg = firstTrip.LegList.Leg;
+              trip.push(leg.Origin.time + ' ' + leg.Origin.name + ' (' + leg.name + ')\n|');
+              trip.push(leg.Destination.time + ' ' + leg.Destination.name + '\n');
+            }
 
-          real = real.replace('{key}', keyReal).replace('{id}', id);
+            trip.push('*Total restid: ' + utils.calculateTimeFromMinutes(firstTrip.dur) + '*');
 
-          utils.getDataFromURL(real)
-            .then(function (body) {
+            channel.send(trip.join('\n'));
+          });
+      } else {
+        plats = plats.replace('{key}', keyPlats).replace('{search}', station);
 
-            var metros = body.ResponseData.Metros;
-            var departures = [];
+        utils.getDataFromURL(plats)
+          .then(function (body) {
+            if (body.StatusCode !== 0) {
+              channel.send('Något gick snett');
+              return;
+            }
 
-            metros.forEach(function (metro) {
-              departures.push('*' + metro.LineNumber + ' ' + metro.Destination + '*\n' + metro.DisplayTime);
-            });
+            if (!body.ResponseData[0]) {
+              channel.send('Kan inte hitta någon station med namnet *' + station);
+              return;
+            }
 
-            channel.send('*' + name + '*\n' + departures.join('\n'));
-          });          
-        });
+            var name = body.ResponseData[0].Name;
+            var id = body.ResponseData[0].SiteId;
+
+            real = real.replace('{key}', keyReal).replace('{id}', id);
+
+            utils.getDataFromURL(real)
+              .then(function (body) {
+
+                var metros = body.ResponseData.Metros;
+                var departures = [];
+
+                metros.forEach(function (metro) {
+                  departures.push('*' + metro.LineNumber + ' ' + metro.Destination + '*\n' + metro.DisplayTime);
+                });
+
+                channel.send('*' + name + '*\n' + departures.join('\n'));
+              });
+          });
+      }
     }
   }
 };
--- utilities/utils.js
+++ utilities/utils.js
@@ -58,8 +58,23 @@ function getDataFromURL (url) {
   return deferred.promise;
 }
 
+function calculateTimeFromMinutes (minutes) {
+  if (minutes < 60) {
+    return minutes > 1 ? minutes + ' minuter' : minutes + ' minut';
+  } else if (minutes === 60) {
+    return '1 timme';
+  } else if (minutes > 60) {
+    var hours = Math.floor(minutes / 60);
+    var minutes = Math.floor(minutes / 60 % 1 * 60);
+    minutes = minutes === 1 ? minutes + ' minut' : minutes + ' minuter';
+    return hours === 1 ? hours + ' timme ' + minutes : hours + ' timmar ' + minutes;
+  }
+}
+
+
 module.exports = {
   showHelp: showHelp,
   returnRandom: returnRandom,
-  getDataFromURL: getDataFromURL
+  getDataFromURL: getDataFromURL,
+  calculateTimeFromMinutes: calculateTimeFromMinutes
 };
